e06b7a2cc4a65ff4f3715b65f27de3b88618cf54
lizhiang <lizhiang@espressif.com>
2025-05-14 15:08:08 +0800

    update interrupt handler to work with context switch

--- common/interrupt.c
+++ common/interrupt.c
@@ -1,11 +1,22 @@
 #include "stdint.h"
 
+#define ISR_STACK_SIZE 1024
+
+uint32_t isr_stack[ISR_STACK_SIZE] __attribute__((aligned(16))); // stack for interrupt context
+uint32_t *isr_stack_top = &isr_stack[ISR_STACK_SIZE]; // top of the isr stack, 16-byte aligned
+
+int ctx_swtch_req = 0; // 0: no context switch, 1: context switch
+
 void _global_interrupt_handler(int mcause)
 {
+    ctx_swtch_req = 0; // context switch is not requested for normal external interrupt
+
     if (mcause == 0x80000001) {
+        extern void timer_handler(void);
         timer_handler();
+        ctx_swtch_req = 1; // context switch is requested for timer interrupt
     }
     else {
-        lib_printf("unknown interrupt: %d\r\n", mcause);
+        printf("unknown interrupt: %d\r\n", mcause);
     }
 }
